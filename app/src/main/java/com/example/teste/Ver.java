639571eb9257e882a2604fec8cf0c9b9a48fb158
marioalve <amario@ipvc.pt>
2019-11-26 23:12:11 +0000

    Ver concluido

--- app/src/main/java/com/example/teste/Ver.java
+++ app/src/main/java/com/example/teste/Ver.java
@@ -3,12 +3,21 @@ package com.example.teste;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.teste.db.Contrato;
 import com.example.teste.db.DB;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.Serializable;
 
@@ -17,36 +26,50 @@ public class Ver extends AppCompatActivity implements Serializable {
     SQLiteDatabase db;
     DB mDbHelper;
     Cursor cursor;
-    int id;
+    int id_user;
     TextView caixa1,caixa2,caixa3,caixa4,caixa5,caixa6,caixa7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ver);
-        caixa1 = findViewById(R.id.nome1);
-        caixa2 = findViewById(R.id.numero1);
-        caixa3 = findViewById(R.id.idade1);
-        caixa4 = findViewById(R.id.pais1);
-        caixa5 = findViewById(R.id.codpostal1);
-        caixa6 = findViewById(R.id.email1);
-        caixa7 = findViewById(R.id.genero1);
-        id = getIntent().getExtras().getInt("ver");
-        mDbHelper = new DB(this);
-        db = mDbHelper.getReadableDatabase();
-        cursor = db.query(false, Contrato.Contactos.TABLE_NAME, Contrato.Contactos.PROJECTION,
-                Contrato.Contactos._ID + " = ?", new String[]{id+""},
-                null, null,
-                null, null);
-        //moves the cursor to the first result (when the set is not empty)
-        cursor.moveToFirst();
-        caixa1.setText(cursor.getString(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_NOME)));
-        caixa2.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_NUMERO))));
-        caixa3.setText(String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_IDADE))));
-        caixa4.setText(cursor.getString(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_PAIS)));
-        caixa5.setText(cursor.getString(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_CODIGOPOSTAL)));
-        caixa6.setText(cursor.getString(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_EMAIL)));
-        caixa7.setText(cursor.getString(cursor.getColumnIndexOrThrow(Contrato.Contactos.COLUMN_GENERO)));
-        cursor.close();
+        caixa1   =  findViewById(R.id.nome1);
+        caixa2   = findViewById(R.id.numero1);
+        caixa3   = findViewById(R.id.idade1);
+        caixa4   = findViewById(R.id.pais1);
+        caixa5   = findViewById(R.id.codpostal1);
+        caixa6   = findViewById(R.id.email1);
+        caixa7   = findViewById(R.id.genero1);
+        id_user = getIntent().getExtras().getInt("ver");
+        String url = "https://unhelmeted-mint.000webhostapp.com/myslim/api/contactov/1" ;
+        // Formulate the request and handle the response.
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject job = response.getJSONObject("linha");
+                            caixa1.setText(job.getString("nome"));
+                            caixa2.setText(job.getString("numero"));
+                            caixa3.setText(job.getString("idade"));
+                            caixa4.setText(job.getString("pais"));
+                            caixa5.setText(job.getString("codigopostal"));
+                            caixa6.setText(job.getString("email"));
+                            caixa7.setText(job.getString("genero"));
+                            //joinIdPais(id);
+                        } catch (JSONException ex) {
+                        }//Toast.makeText(getActivity(), response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Ver.this, error.toString(), Toast.LENGTH_SHORT).show();
+                        Log.d("Erro", error.toString());
+                    }
+                });
+        MySingleton.getInstance(Ver.this).addToRequestQueue(jsObjRequest);
+
+
+
     }
 }
